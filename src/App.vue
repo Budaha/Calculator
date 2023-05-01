0358e50e3c5a5a3ce13c8a127d8f88f5283aa57f
Andrey <budahinan@gmail.com>
2023-05-01 20:36:47 +0300

    калькулятор

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
 <div class="container">   
-    <div class="grid">
-        <input type = "text" v-model= "result" placeholder="0">
+    <div class="calc">
+        <input type="text" v-model="result" placeholder="0">
         <button @click="input(num)" class="cell num" v-for="num in numbers" :key="num">{{ num }}</button>
         <button @click="input(op)" class="cell op" v-for="op in operations" :key="op">{{ op }}</button>
-        <button @click="reset()" class="cell op">R</button>
-     <button @click="calc()" class="cell op">=</button>
+        <button @click="reset()" class="cell op">AC</button>
+        <button @click="calc()" class="cell op">=</button>
     </div>
 </div>    
 </template>
+
 <script>
-
-
 export default {
     data() {
         return {
@@ -19,7 +18,7 @@ export default {
         numbers: [1,2,3,4,5,6,7,8,9,0],
         operations: ['+', '-', '*', '/'],
     }
-    },
+},
     methods: {
         input: function(char) {
             this.result = this.result.toString();
@@ -31,26 +30,26 @@ export default {
         calc: function() {
             this.result = eval(this.result);
         },
-    }
-
+    },
 }
 </script>
 
 
 <style>
 .container {
-    width: 200px;
-    box-shadow: 0 3px 15px grey;
+    width: 250px;
+    box-shadow: 0 5px 15px gray;
 }
-input[type="text"] {
-    width: 200px;
+input {
+    width: 100%;
     border: 0;
     color: #243849;
     font-size: 18px;
+    height: 40px;
     padding: 5px 5px;
     text-align: right;
 }
-.grid {
+.calc {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
@@ -63,12 +62,12 @@ input[type="text"] {
     border: none;
 }
 .num {
-    background-color: #243849 ;
-    color: #fff;
+    background-color: black;
+    color: white;
     font-weight: 400;
 }
 .cell:hover {
-    background-color: #182530;
+    background-color: gray;
 }
 .op {
     background-color: orange;
